fa6e9025f764e1a4a8b4b73a45a10218a542f803
Chinyiskan <david.camilo.charry@gmail.com>
2025-05-07 17:53:57 -0500

    Add SVG icons and HTML templates for energy efficiency calculator
    - Added home.svg and light.svg icons for the application.
    - Introduced planet_bad.svg, planet_good.svg, and planet_medium.svg icons to represent energy consumption results.
    - Created base.html as the main template structure for the application.
    - Developed electronics.html, end.html, index.html, and lights.html templates for user interaction and results display.
    - Updated Pipfile to include jinja2 package for template rendering.

---
+++ M3/L2/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+def result_calculate(size: int, lights: int, devices: int) -> float:
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5
+    return size * home_coef + lights * light_coef + devices * devices_coef
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/size/<int:size>')
+def select_lights(size):
+    return render_template('lights.html', size=size)
+
+@app.route('/size/<int:size>/lights/<int:lights>')
+def select_devices(size, lights):
+    return render_template('electronics.html', size=size, lights=lights)
+
+@app.route('/size/<int:size>/lights/<int:lights>/devices/<int:devices>')
+def show_result(size, lights, devices):
+    result = result_calculate(size, lights, devices)
+    return render_template('end.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
